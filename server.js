982994495d50b630d4e9aa704db3931ece86859d
jenniferchristine <jeja2306@student.miun.se>
2024-06-07 15:56:42 +0200

    activated packages and created connection to port

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+// starta
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
