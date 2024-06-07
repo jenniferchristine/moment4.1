7f6151efc29cfc09cfb0e1fba92d6e7d5d7d10d3
jenniferchristine <jeja2306@student.miun.se>
2024-06-07 17:33:30 +0200

    connected to mongodb, installed bcrypt and started a schema for users

---
+++ models/user.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// schema för användare
+const userSchema = new mongoose.Schema({
+
+});
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// anslutning
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Connected to MongoDB...");
+}).catch((error) => {
+    console.error("Error when connecting to database...");
+});
+
+// modell för användare
+const user = require("..models/user");
 
 // ny användare
 router.post("/register", async (req, res) => {
-    console.log("Register called...");
+    try {
+        const { username, password, firstname, lastname } = req.body; // användaruppgifter
+        if (!username || !password || !firstname || !lastname) { // validering
+            return res.status(400).json({ error: "Invalid input, all fields required" });
+        }
+        res.status(201).json({ message: "User created" });
+    } catch (error) {
+        res.status(500).json({ error: "Server error" })
+    }
 });
 
 // login för användare
 router.post("/login", async (req, res) => {
-    console.log("Login called...");
+    try {
+        const { username, password } = req.body; // inloggningsuppgifter
+        if (!username || !password ) { // validering
+            return res.status(400).json({ error: "All fields required" });
+        }
+        if (username === "jeja" && password === "password123") {
+        res.status(201).json({ message: "Login successful" });
+    } else {
+        res.status(401).json({ error: "Invalid username or password"});
+    }
+    } catch (error) {
+        res.status(500).json({ error: "Server error" })
+    }
 });
 
 module.exports = router; 
