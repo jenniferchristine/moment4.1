cc5b740bbb95beec9d23b2fbe920c07184281c60
jenniferchristine <jeja2306@student.miun.se>
2024-06-07 15:58:27 +0200

    created use and file for routes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
+// routes
+app.use("/api", authRoutes);
+
 // starta
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
