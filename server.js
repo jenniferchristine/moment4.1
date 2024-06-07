3daa2c3b166a3eec0de9c33b2037f8841162f849
jenniferchristine <jeja2306@student.miun.se>
2024-06-08 01:37:34 +0200

    installed cors (dont know if needed yet) and updated README

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
+app.use(cors());
 
 // routes
 app.use("/api", authRoutes);
