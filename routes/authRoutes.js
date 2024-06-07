0a76791e2b96afdc04709d9e4790fc00bc33a343
jenniferchristine <jeja2306@student.miun.se>
2024-06-07 20:37:54 +0200

    installed and applied jwt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 // anslutning
@@ -44,7 +45,13 @@ router.post("/login", async (req, res) => {
         if (!isPasswordMatch) {
             return res.status(401).json({ error: "Incorrect username or password" });
         } else {
-            res.status(201).json({ message: "Successful login!" });
+            const payload =  { username: username };
+            const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+            const response = {
+                message: "Login successful",
+                token: token
+            }
+            res.status(200).json({ response });
         }
     } catch (error) {
         res.status(500).json({ error: "Server error" })
